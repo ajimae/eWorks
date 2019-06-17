1db8cca141262ed10b0cebd75ce00e70cd18f2da
ajimae <meeky.ae@gmail.com>
2019-06-17 03:02:06 +0100

    feat(authentication): authenticate and encrypt details
    - add jsonwebtoken library for generating user token on signup
    - add bcryptjs library to encrypt user password on signup
    - add functionalities (logic) to user model
    
    [Finishes #000002]

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   firstName: String,
@@ -21,9 +22,37 @@ const userSchema = new mongoose.Schema({
   },
   isActive: {
     type: Boolean,
-    required: true,
+    required: false,
     default: false,
   }
+});
+
+/**
+ * @description encrypt and store user password
+ *
+ * @param save, callback
+ */
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // generate a hashing salt
+  bcrypt.genSalt(10, function(error, salt) {
+    if (error) {
+      throw new Error('error occurred while encryting password');
+    }
+
+    // hash password using generated salt
+    bcrypt.hash(user.password, salt, function(error, hash) {
+      if (error) {
+        throw new Error('an error occurred while encryting password');
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 const User = mongoose.model('User', userSchema);
